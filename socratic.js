a576ed474bbea2c58954f4473d0a8475c3bd3b73
Boris Caspary <boris.caspary@freenet.de>
2026-09-11 21:53:10 +0200

    added animated response

--- socratic.js
+++ socratic.js
@@ -6,12 +6,50 @@ button.addEventListener("click", updateButton);
 
 function updateButton() {
 	if(!chatInput.value) {
-		return
+		return;
 	}
+	text = chatInput.value;
+	sendMessage(text, true);
+	respond(text);
+	chatInput.value="";
+}
+
+async function respond(text) {
+	response = "I hear you. Tell me more.";
+	msg = sendMessage("", false);
+	await thinking(msg);
+	typeText(response, msg);
+}
+
+function sendMessage(text, user) {
 	msg = document.createElement("p");
-	msg.textContent = chatInput.value;
-	msg.setAttribute("class", "user");
+	msg.textContent = text;
+	if(user) {
+		msg.setAttribute("class", "user");
+	} else {
+		msg.setAttribute("class", "bot")
+	}
 	chatDisplay.appendChild(msg);
 	chatDisplay.scrollTo(0, chatDisplay.scrollHeight);
-	chatInput.value="";
+
+	return msg;
+}
+
+async function thinking(parent) {
+	bubbles = ["Ooo", "oOo", "ooO"];
+	for (var i = 0; i <= 11; i++) {
+		parent.textContent = bubbles[i%bubbles.length];
+		await sleep(300);
+	}
+}
+
+async function typeText(text, parent) {
+	for (var i = 0; i <= text.length; i++) {
+		parent.textContent = text.slice(0, i);
+		await sleep(Math.random()*100);
+	}
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
